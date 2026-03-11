2112f4cc9610cb852f39aa0b51f1d4f24df9b799
Sissoko123 <moussatiecourasissoko@gmail.com>
2026-03-11 08:08:30 +0000

    Premier commit : Système d'inscription

---
+++ actions/database.php
@@ -0,0 +1,6 @@
+<?php
+try{
+    $bdd = new PDO('mysql:host=127.0.0.1;port=3309;dbname=forum;charset=utf8;', 'root', '');
+}catch(Exception $e){
+    die('Une erreur a été trouvée : ' . $e->getMessage());
+}
